8039247fc995b2112f8973b29683df935068d9ab
sputnik1458 <sputnik1@protonmail.com>
2016-07-07 20:22:33 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp -r ${USER_HOME}/Cryptocurrency-Ticker/.conkyrc ${USER_HOME}
 chmod +x ${USER_HOME}/Cryptocurrency-Ticker/cryptocurrency-ticker.py
 
 crontab -l > mycron
-echo "*/5 * * * * ${USER_HOME}/Cryptocurrency-Ticker/cryptocurrency-ticker.py" >> mycron
+echo "*/5 * * * * ${USER_HOME}/cryptocurrency-ticker-master/cryptocurrency-ticker.py" >> mycron
 crontab mycron
 rm mycron       
 
